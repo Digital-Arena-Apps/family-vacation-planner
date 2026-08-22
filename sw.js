5b8d9c270bf71cd6c6a0e96c7bcfc03f582474f9
Digital-Arena-Apps <blackmarehouse@gmail.com>
2026-08-22 23:13:33 +0100

    Bump cache for clean onboarding demo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ffvp-v2-5-2';
+const CACHE='ffvp-v2-5-3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./brand-mark.png','./brand-logo.png','./landing-scenic.png','./decision-demo-loader.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
